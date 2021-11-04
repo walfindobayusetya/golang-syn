a0b64582ea9f5c721d23405997b0eeba591ea3bb
walfindobayusetya <walfindobayusetya23.wbs@gmail.com>
2021-11-04 17:55:57 +0700

    Update Module

--- module.go
+++ module.go
@@ -1,5 +1,12 @@
-package golangsyn
+package main
+
+import "fmt"
 
 func Walfindo() string{
 	return "Golang Module Project"
+}
+
+func Penjumlahan(a, b int){
+	hasil := a + b
+	fmt.Println(hasil)
 }
